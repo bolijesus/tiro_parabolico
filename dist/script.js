c586bdba1a3c9a121fe2614d1298325480c808fb
bolijesus <bolijesus98@gmail.com>
2023-05-07 08:41:25 -0500

    1ra refacrotizacion funcional

--- dist/script.js
+++ dist/script.js
@@ -1,118 +1,135 @@
-var velocidadInicial_X;
-var velocidadInicial_Y;
+var velocidadInicialX;
+var velocidadInicialY;
 var tiempoSubida;
 var tiempoTotal;
-var x;
-var y;
-var tiempo;
-var beginmove;
-var g = 9.8;
-var xmax;
-var ymax;
+var posicionHorizontal;
+var posicionVertical;
+var tiempoTranscurrido;
+var intervaloMovimiento;
+var gravedad = 9.8;
+var distanciaHorizontalMaxima;
+var alturaMaxima;
 var posTop;
 
 function resolve() {
-    clearInterval(beginmove);
-    var vi = document.getElementById("vi").value;
-    var theta = document.getElementById("theta").value;
+    var velocidadInicial = document.getElementById("vi").value;
+    var anguloLanzamiento = document.getElementById("theta").value;
     let hr = document.getElementsByTagName('hr'); 
     var area = document.getElementById("area");
-    g = document.getElementById("gravedad").value;
-    theta = theta * (Math.PI / 180);
+    gravedad = document.getElementById("gravedad").value;
+    anguloLanzamiento = anguloLanzamiento * (Math.PI / 180);
 
     var imgs = area.querySelectorAll("img");
     for (var i = 0; i < imgs.length; i++) {
         area.removeChild(imgs[i]);
     }
-    
-    //vix,viy
-    velocidadInicial_X = vi * Math.cos(theta).toFixed(3);
-    velocidadInicial_Y = vi * Math.sin(theta).toFixed(3);
 
-    //ts,tt
-    tiempoSubida = (velocidadInicial_Y / g).toFixed(3);
+    // Cálculo de las componentes de la velocidad inicial en X y Y
+    velocidadInicialX = velocidadInicial * Math.cos(anguloLanzamiento).toFixed(3);
+    velocidadInicialY = velocidadInicial * Math.sin(anguloLanzamiento).toFixed(3);
+
+    // Cálculo del tiempo de subida y tiempo total de vuelo
+    tiempoSubida = (velocidadInicialY / gravedad).toFixed(3);
     tiempoTotal = (tiempoSubida * 2).toFixed(3);
 
-    //ymax,xmax
-    xmax = (velocidadInicial_X * tiempoTotal).toFixed(3);
-    ymax = ((Math.pow(velocidadInicial_Y, 2) / 2) * g).toFixed(3);
+    // Cálculo de la distancia horizontal máxima y altura máxima
+    distanciaHorizontalMaxima = (velocidadInicialX * tiempoTotal).toFixed(3);
+    alturaMaxima = ((Math.pow(velocidadInicialY, 2) / 2) * gravedad).toFixed(3);
     
-    for (tiempo = 0; tiempo <= tiempoTotal; tiempo += 0.5) {
-        x = velocidadInicial_X * tiempo;
-        y = 400 - (velocidadInicial_Y * tiempo - (g * Math.pow(tiempo, 2)) / 2);
-        var ball = document.createElement("img");
-        var vy = Math.abs(velocidadInicial_Y - g * tiempo);
-        var v = Math.sqrt(Math.pow(velocidadInicial_X, 2) + Math.pow(vy, 2));
-        ball.src = 'https://static.vecteezy.com/system/resources/previews/016/314/339/non_2x/red-circle-red-dot-icon-free-png.png';
-        ball.style.width = 48 + "px";
-        ball.style.position = "absolute";
-        ball.style.top = (hr[0].offsetTop-447+y) + "px";
-        ball.style.left = x + "px";
-        ball.title = `velx:${velocidadInicial_X},vely:${vy},vel:${v}`;
+    // Generar las imágenes de la pelota para cada instante de tiempo
+    for (tiempoTranscurrido = 0; tiempoTranscurrido <= tiempoTotal; tiempoTranscurrido += 0.5) {
+        posicionHorizontal = velocidadInicialX * tiempoTranscurrido;
+        posicionVertical = 400 - (velocidadInicialY * tiempoTranscurrido - (gravedad * Math.pow(tiempoTranscurrido, 2)) / 2);
+        var pelota = document.createElement("img");
+        var velocidadY = Math.abs(velocidadInicialY - gravedad * tiempoTranscurrido);
+        var velocidad = Math.sqrt(Math.pow(velocidadInicialX, 2) + Math.pow(velocidadY, 2));
+        pelota.src = 'https://static.vecteezy.com/system/resources/previews/016/314/339/non_2x/red-circle-red-dot-icon-free-png.png';
+        pelota.style.width = 48 + "px";
+        pelota.style.position = "absolute";
+        posTop = hr[0].offsetTop - 447 + posicionVertical;
+        pelota.style.top = posTop + "px";
+        pelota.style.left = posicionHorizontal + "px";
+        pelota.title = `velx:${velocidadInicialX},vely:${velocidadY},vel:${velocidad}`;
 
-        area.appendChild(ball);
-    }
-    img.style.left = xmax + "px";
-    img.style.top = "400px";
+        area.appendChild(pelota);
+    }    
 }
 
-function startmove() {
-    clearInterval(beginmove);
+
+function startmove() {    
+
+    // Obtiene los valores de entrada del usuario
+    var velocidadInicial = parseFloat(document.getElementById("vi").value);
+    var anguloLanzamiento = parseFloat(document.getElementById("theta").value);
+    gravedad = parseFloat(document.getElementById("gravedad").value);
     
-    var info = document.getElementById('info')
-    var vi = document.getElementById("vi").value;
-    var theta = document.getElementById("theta").value;
-    g = document.getElementById("gravedad").value;
+
+    // Convierte el ángulo de lanzamiento de grados a radianes
+    var anguloRadianes = anguloLanzamiento * (Math.PI / 180);
+
+    // Calcula las componentes de la velocidad inicial en X y Y
+    velocidadInicialX = (velocidadInicial * Math.cos(anguloRadianes)).toFixed(3);
+    velocidadInicialY = (velocidadInicial * Math.sin(anguloRadianes)).toFixed(3);
+
+    // Calcula el tiempo de subida y el tiempo total de vuelo
+    tiempoSubida = (velocidadInicialY / gravedad).toFixed(3);
+    tiempoTotal = (tiempoSubida * 2).toFixed(3);
+
+    // Calcula la altura máxima alcanzada y la distancia horizontal máxima
+    distanciaHorizontalMaxima = (velocidadInicialX * tiempoTotal).toFixed(3);
+    alturaMaxima = ((Math.pow(velocidadInicialY, 2) / 2) * gravedad).toFixed(3);
+
+    // Reinicia el tiempo transcurrido a cero
+    tiempoTranscurrido = 0;
+
+    // Crea el intervalo de tiempo para actualizar la posición de la pelota
+    intervaloMovimiento = setInterval(move, 10);
+
+    // Limpia cualquier imagen anterior
     var area = document.getElementById("area");
-    theta = theta * (Math.PI / 180);
-
-    //vix,viy
-    velocidadInicial_X = vi * Math.cos(theta).toFixed(3);
-    velocidadInicial_Y = vi * Math.sin(theta).toFixed(3);
-
-    //ts,tt
-    tiempoSubida = (velocidadInicial_Y / g).toFixed(3);
-    tiempoTotal = (tiempoSubida * 2).toFixed(3);
-    //ymax,xmax
-    xmax = (velocidadInicial_X * tiempoTotal).toFixed(3);
-    ymax = ((Math.pow(velocidadInicial_Y, 2) / 2) * g).toFixed(3);
-    tiempo = 0;
-
-    beginmove = setInterval(move, 10);
-    info.className = 'card1'
-    var imgs = area.querySelectorAll("img");
-    for (var i = 0; i < imgs.length; i++) {
-        area.removeChild(imgs[i]);
+    var imagenes = area.querySelectorAll("img");
+    for (var i = 0; i < imagenes.length; i++) {
+        area.removeChild(imagenes[i]);
     }
-    
-    info.innerHTML =
-        `
-    Vi = ${vi}<br>
-    Theta = ${theta.toFixed(3)} rad<br>
-    Vix = ${velocidadInicial_X}<br>
-    Viy = ${velocidadInicial_Y}<br>
-    Ts = ${tiempoSubida}<br>
-    Tt = ${tiempoTotal}<br>
-    Xmax = ${xmax}<br>
-    Ymax = ${ymax}<br>
-    `
+
+    // Actualiza la información mostrada al usuario
+    var info = document.getElementById("info");
+    info.className = "card1";
+    info.innerHTML = `
+        Vi = ${velocidadInicial}<br>
+        Theta = ${anguloLanzamiento.toFixed(3)} rad<br>
+        Vix = ${velocidadInicialX}<br>
+        Viy = ${velocidadInicialY}<br>
+        Ts = ${tiempoSubida}<br>
+        Tt = ${tiempoTotal}<br>
+        Xmax = ${distanciaHorizontalMaxima}<br>
+        Ymax = ${alturaMaxima}<br>
+    `;
 }
-function move() {    
+
+function move() {
+    // Obtiene los elementos relevantes de la página
+    var hr = document.getElementsByTagName("hr");
+    var pelota = document.getElementById("ball");
+
+    // Si todavía no se ha alcanzado el tiempo total de vuelo
+    if (tiempoTranscurrido <= tiempoTotal) {
+        // Calcula la posición de la pelota en el tiempo actual
+        posicionHorizontal = velocidadInicialX * tiempoTranscurrido;
+        posicionVertical = 400 - (velocidadInicialY * tiempoTranscurrido - (gravedad * Math.pow(tiempoTranscurrido, 2)) / 2);
+
+        // Actualiza la posición de la pelota en la página
+        pelota.style.left = posicionHorizontal + "px";
+        pelota.style.top = (hr[0].offsetTop - 447 + posicionVertical) + "px";
+        
+    }else{
+        resolve();
+        clearInterval(intervaloMovimiento);
+    }    
+
+    // Incrementa el tiempo transcurrido
+    tiempoTranscurrido += 0.1;
     
-    let hr = document.getElementsByTagName('hr');
-
-    var ball = document.getElementById("ball");
-    var puntos = document.getElementById('puntos')
-
-    if (tiempo <= tiempoTotal) {
-
-        x = velocidadInicial_X * tiempo;
-        y = 400 - (velocidadInicial_Y * tiempo - (g * Math.pow(tiempo, 2)) / 2);
-        ball.style.left = x + "px";
-        ball.style.top = (hr[0].offsetTop - 447+y) + "px";
-
-    }
-    tiempo += 0.1;   
 }
 
 function positiontop() {
@@ -124,5 +141,5 @@ function positiontop() {
     if (posTop < 50) {
         posTop = 50
     }
-    console.log(posTop)
+    
 }
